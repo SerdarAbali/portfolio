8708e2e100ce443c484d5811f59241621034bd1f
Serdar <serdar@live.fi>
2026-09-08 22:02:48 +0300

    fix: dynamic basePath for github pages

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,16 @@
 import type { NextConfig } from "next";
 
+const repoName = process.env.GITHUB_REPOSITORY
+  ? `/${process.env.GITHUB_REPOSITORY.split("/")[1]}`
+  : "";
+
 const nextConfig: NextConfig = {
   output: "export",
   images: {
     unoptimized: true,
   },
+  basePath: repoName,
+  assetPrefix: repoName,
 };
 
 export default nextConfig;
